7ad344b6ca73ccf1184dca268da73bd629671c16
duyux <duyux@163.com>
2017-07-15 11:22:41 +0800

    js学习

---
+++ jsHello.js
@@ -0,0 +1,20 @@
+﻿function Person(name, age, job)
+{
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.friends = ["Shelby","Count"];
+}
+Person.prototype = {
+    constructor:Person,
+    sayName:function(){
+    alert(this.name);
+}
+}
+
+
+
+function sayHi() {
+    var person1 = new Person("Michase",20,"Soft");
+    person1.sayName();
+}
